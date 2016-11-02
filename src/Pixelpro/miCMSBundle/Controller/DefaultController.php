cf7fd04ce5d6c4741d19862dc26238bc1a518c85
davidpiq <info@pixelpro.es>
2016-11-02 17:28:55 +0100

    Add files via upload

--- src/Pixelpro/miCMSBundle/Controller/DefaultController.php
+++ src/Pixelpro/miCMSBundle/Controller/DefaultController.php
@@ -3,11 +3,30 @@
 namespace Pixelpro\miCMSBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PixelpromiCMSBundle:Default:index.html.twig');
-    }
+
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('PixelpromiCMSBundle:Post')->findAll();
+        return $this->render('PixelpromiCMSBundle:Default:index.html.twig', array('post'=>$post));
+      }
+
+
+      public function showAction($id)
+      {
+          $em = $this->getDoctrine()->getManager();
+          $ppost = $em->getRepository('PixelpromiCMSBundle:Post')->find($id);
+          return $this->render('PixelpromiCMSBundle:Default:show.html.twig', array('ppost'=>$ppost));
+        }
+
+      public function langAction(Request $request){
+        return $this->redirectToRoute("pixelpromi_cms_homepage");
+      }
+
 }
---
+++ src/Pixelpro/miCMSBundle/Form/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pixelpro\miCMSBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password', PasswordType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Pixelpro\miCMSBundle\Entity\User'
+        ));
+    }
+}
